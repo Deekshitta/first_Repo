419673f6910674162f770ce8c8f50bcad58f6e49
Deekshitta <sudhakaran.govin@gmail.com>
2026-07-07 12:18:01 +0530

    Update main.tf

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0f1a1e5dc0c29441c"
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
 
   tags = {
