3b3f294be0d8393df4430e365d848dd581aa5653
Deekshitta <sudhakaran.govin@gmail.com>
2026-07-10 19:45:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
-
 provider "aws" {
   region = "ap-southeast-2"
 }
 
 resource "aws_instance" "ec2" {
+  count         = 3
   ami           = "ami-06259b63260eddc13"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "GitHub-AWSEC2001"
+    Name = "GitHub-AWSEC2-${count.index + 1}"
   }
 }
