2b97bc4959513c275140876ae5d95a5f44b3d11c
Deekshitta <sudhakaran.govin@gmail.com>
2026-07-14 13:03:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "firsts3-sudha"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
@@ -6,9 +16,6 @@ resource "aws_instance" "ec2" {
   count         = 1
   ami           = "ami-06259b63260eddc13"
   instance_type = "t3.micro"
-resource "aws_s3_bucket" "stage" {
-  bucket = "my-unique-bucket-name-12345"
-}
 
   tags = {
     Name = "GitHub-AWSEC2-${count.index + 1}"
