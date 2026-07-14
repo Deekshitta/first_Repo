e00b763f986157fe464f4c4af5e9c8699ac2c9da
Deekshitta <sudhakaran.govin@gmail.com>
2026-07-14 12:56:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "ec2" {
   count         = 1
   ami           = "ami-06259b63260eddc13"
   instance_type = "t3.micro"
+resource "aws_s3_bucket" "stage" {
+  bucket = "my-unique-bucket-name-12345"
+}
 
   tags = {
     Name = "GitHub-AWSEC2-${count.index + 1}"
